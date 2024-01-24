767b49952d6846115ad70778c452bfac8a20968d
Tomas Catalano <74085420+TomasC753@users.noreply.github.com>
2024-01-24 17:30:21 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,12 +16,12 @@ const config = {
   url: 'https://TomasC753.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/apuntes-dart',
+  baseUrl: '/apuntes',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'TomasC753', // Usually your GitHub org/user name.
-  projectName: 'apuntes-dart', // Usually your repo name.
+  projectName: 'apuntes', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
